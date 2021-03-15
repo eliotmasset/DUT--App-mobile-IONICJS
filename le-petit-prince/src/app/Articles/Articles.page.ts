79d47c6589a7d567bd485a735ff824a1e04c7127
LeProdrome <58653158+EliotMasset@users.noreply.github.com>
2021-03-15 18:02:32 +0100

    add favs storage

--- le-petit-prince/src/app/Articles/Articles.page.ts
+++ le-petit-prince/src/app/Articles/Articles.page.ts
@@ -12,7 +12,9 @@ export class ArticlesPage {
 
   datas:any;
   images:any;
-  
+  fav: boolean;
+  favs: any;
+
   async refresh(e){
     this.storage.get('login').then((valeur) => {
       if(valeur=="")
@@ -27,7 +29,47 @@ export class ArticlesPage {
     });
   }
 
+  update() {
+    this.fav=!this.fav;
+  }
+
+  canBeDisplay(article) {
+    if(!this.fav)
+      return true;
+    else
+    { 
+      if(this.favs.indexOf(article)==-1)
+        return false;
+      return true;
+    }
+  }
+
+  add_tofavs(fav) {
+    if(!this.isInFavs(fav))
+    { 
+      this.favs.push(fav);
+    }
+    else
+      this.favs=this.favs.filter(function(value, index, arr){ 
+        return JSON.stringify(value) != JSON.stringify(fav);
+    });
+    this.storage.set('favs', this.favs);
+  }
+
+  isInFavs(article) {
+    for(let fav of this.favs)
+      if(JSON.stringify(fav)==JSON.stringify(article))
+        return true;
+    return false;
+  }
+
   constructor(private router: Router,private storage: Storage,private http: HttpClient) {
+    this.favs=[];
+    
+    storage.get('favs').then((valeur) => {
+      if(valeur!=null)
+        this.favs=valeur;
+    });
     storage.get('login').then((valeur) => {
       if(valeur=="")
         this.router.navigate(['/']);
